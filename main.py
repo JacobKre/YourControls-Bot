5d480ce3a14faa9a7241861c3bcdc32120362a6b
JacobK <techsupport@kreindler.ca>
2021-11-30 19:09:54 -0500

    added cog loader and unloader

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import discord
 import random
 import asyncio
+from discord.ext import commands
 from discord import message
 from discord.utils import get
 from discord_slash import SlashCommand
@@ -11,14 +12,22 @@ from discord_slash.utils.manage_commands import create_option
 load_dotenv()
 token = os.getenv('TOKEN')
 
-client = discord.Client(intents=discord.Intents.all(), status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="YourControls v2.6.3"))
-slash = SlashCommand(client, sync_commands=True)
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all(), status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="YourControls v2.6.3"))
+slash = SlashCommand(bot, sync_commands=True)
 
 guild_ids = [764805300229636107]
 ready = False
 
+#Load Cogs
+@bot.command()
+async def load(ctx, extention):
+    bot.load_extension(f'cogs.{extention}')
+#Unload Cogs
+@bot.command()
+async def unload(ctx, extention):
+    bot.unload_extension(f'cogs.{extention}')
 
-@client.event
+@bot.event
 async def on_ready():
     global ready
     print("Ready!")
@@ -26,10 +35,10 @@ async def on_ready():
 
 @slash.slash(name="ping", description="Shows bot latency", guild_ids=guild_ids)
 async def _ping(ctx):
-    await ctx.send(f"🌍 Ping is `{round(client.latency * 1000)}ms`")
+    await ctx.send(f"🌍 Ping is `{round(bot.latency * 1000)}ms`")
 
 
 
 
 
-client.run(token)
+bot.run(token)
