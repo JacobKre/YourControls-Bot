8394f0c63c3fb0aa3b52fd8a7d4ca0a6ef7af579
JacobK <techsupport@kreindler.ca>
2021-11-30 19:04:25 -0500

    added a bunch of boring stuff

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 import os
 from dotenv import load_dotenv
 import discord
+import random
+import asyncio
+from discord import message
+from discord.utils import get
+from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option
 
 load_dotenv()
 token = os.getenv('TOKEN')
+
+client = discord.Client(intents=discord.Intents.all(), status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="YourControls v2.6.3"))
+slash = SlashCommand(client, sync_commands=True)
+
+guild_ids = [764805300229636107]
+ready = False
+
+
+@client.event
+async def on_ready():
+    global ready
+    print("Ready!")
+    ready = True
+
+@slash.slash(name="ping", description="Shows bot latency", guild_ids=guild_ids)
+async def _ping(ctx):
+    await ctx.send(f"🌍 Ping is `{round(client.latency * 1000)}ms`")
+
+
+
+
+
+client.run(token)
