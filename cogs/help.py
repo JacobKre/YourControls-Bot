9fb959470a64f3d1616251f14b5c1a18074318ef
JacobK <techsupport@kreindler.ca>
2022-07-27 22:32:37 -0400

    feat: updated to the interactions library

--- cogs/help.py
+++ cogs/help.py
@@ -1,58 +1,55 @@
-import discord
-from discord.ext import commands
-from discord_slash import cog_ext, SlashContext
-
+import interactions
 
 guild_ids = [764805300229636107]
 
 
-class help(commands.Cog):
+class help(interactions.Extension):
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: interactions.Client = bot
 
-    @cog_ext.cog_slash(name="help", description="Shows list of commands", guild_ids=guild_ids)
+    @interactions.extension_command(name="help", description="Shows list of commands", scope=guild_ids)
     async def _help(self, ctx):
-        embed=discord.Embed(color=0x00d0ff)
+        embed=interactions.Embed(color=0x00d0ff)
         embed.add_field(name = "__**Help**__", value = "\u200b\n`/modhelp` Gives a list of all the moderation commands available\n\n`/aircraft` Gives you a list of the aircraft supported by YourControls\n\n`/install` Shows the instructions on how to install YourControls\n\n`/brokengauge` Shows the list of possible fixes for the Could not connect to the YourControls gauge\n\n`/vatsim` Shows the instructions on how to use YourControls with Vatsim/IVAO\n\n`/nomsg` Shows possible fixes for the No Message Recived From Server error")
-        await ctx.send(embed=embed)
+        await ctx.send(embeds=embed)
 
-    @cog_ext.cog_slash(name="modhelp", description="Shows list of moderation commands", guild_ids=guild_ids)
+    @interactions.extension_command(name="modhelp", description="Shows list of moderation commands", scope=guild_ids)
     async def _modhelp(self, ctx):
-        embed=discord.Embed(color=0x00d0ff)
+        embed=interactions.Embed(color=0x00d0ff)
         embed.add_field(name = "__**Moderation Commands**__", value = "\u200b\n`/ban` Bans a user\n\n`/kick` Kicks a user\n\n`/mute` Mutes a user\n\n`/unmute` Unmutes a user\n\n`/clear` Clears a number of messages\n\n`/warn` Warns a user")
-        await ctx.send(embed=embed)
+        await ctx.send(embeds=embed)
 
-    @cog_ext.cog_slash(name="aircraft", description="Lists supported aircraft", guild_ids=guild_ids)
+    @interactions.extension_command(name="aircraft", description="Lists supported aircraft", scope=guild_ids)
     async def _aircraft(self, ctx):
-        embed=discord.Embed(color=0x00d0ff)
+        embed=interactions.Embed(color=0x00d0ff)
         embed.add_field(name = "__**Aircraft Supported By YourControls**__", value = "\u200b\n`• All Stock Asobo Aircraft`\n`• AeroplaneHeaven C140`\n`• AeroplaneHeaven Electra-10A`\n`• Aerosoft CRJ`\n`• Aerosoft TwinOtter`\n`• BigRadials JRF-6B Goose`\n`• Carenado C170B`\n`• Carenado M20R Ovation`\n`• Carenado PA34T Seneca V`\n`• DCDesigns Concorde`\n`• Deejing RV-7`\n`• FlyByWire A32NX Stable`\n`• FlyByWire A32NX Development`\n`• FlyByWire A32NX Experimental `\n`• FlyInside Bell 47G`\n`• Flysimware C414AW Chancellor`\n`• Headwind A330-900neo`\n`• Heavy Division 78XH `\n`• HypePerformanceGroup H135`\n`• IRIS G115 T.1 Tutor `\n`• IRIS J160-J170 Jabiru`\n`• JPLogistics C152`\n`• JustFlight PA28R Arrow `\n`• JustFlight PA28R TurboArrow`\n`• JustFlight PA28 Warrior`\n`• Magraina C208B EX `\n`• Milviz Porter `\n`• mixMugz TBM930`\n`• Mrtommymxr C172 `\n`• Mrtommymxr DA40NGX`\n`• Mrtommymxr DA62X`\n`• Parallel42 KitFox STi`\n`• PMDG DC6A-B`\n`• RotorSimPilot H125`\n`• RotorSimPilot R44 `\n`• SaltySimulations 747-8i`\n`• SimSkunkWorks TF-104G`")    
         embed.add_field(name = "\u200b", value = "\u200b\n`• SimWorks Kodiak 100 `\n`• SimWorks RV-14`\n`• TheFrett BonanzaG36 `\n`• WorkingTitle CJ4`\n`• Wing42 Boeing 247D`")    
-        await ctx.send(embed=embed, hidden=True)
+        await ctx.send(embeds=embed, ephemeral=True)
 
-    @cog_ext.cog_slash(name="install", description="Shows how to install YourControls", guild_ids=guild_ids)
+    @interactions.extension_command(name="install", description="Shows how to install YourControls", scope=guild_ids)
     async def _install(self, ctx):
-        embed=discord.Embed(color=0x00d0ff)
+        embed=interactions.Embed(color=0x00d0ff)
         embed.add_field(name = "__**How To Install YourControls**__", value = "There are two ways to install YourControls either using the installer or by installing it manually. \n\n**Install Using The Installer**\nDownload and run the [installer](https://github.com/sequal32/yourcontrolsinstaller/releases/latest/download/installer.zip). If the installer doesn't launch then you may need to install [WebView2](https://go.microsoft.com/fwlink/p/?LinkId=2124703)\n\n**Manual Install**\nIf you would prefer to install it manually then please follow the instructions listed [here](https://docs.yourcontrols.one/installing#manual-installation).")
-        await ctx.send(embed=embed)
+        await ctx.send(embeds=embed)
 
-    @cog_ext.cog_slash(name="brokengauge", description="Shows possible fixes for the Could Not Connect To The YourControls Gauge error", guild_ids=guild_ids)
+    @interactions.extension_command(name="brokengauge", description="Shows possible fixes for the Could Not Connect To The YourControls Gauge error", scope=guild_ids)
     async def _gauge(self, ctx):
-        embed=discord.Embed(color=0x00d0ff)
+        embed=interactions.Embed(color=0x00d0ff)
         embed.add_field(name = "__**How To Fix Broken YourControls Gauge**__", value = "If you're getting an error which says `Could Not Connect To The YourControls Gauge` one possibility for why YourControls can't connect to the gauge is because when YourControls was installed all or parts of the app were installed to the wrong location.\n\n**Step 1:**\nExit Microsoft Flight Simulator if it's running\n\n**Step 2:**\nDownload, extract and run the installer from [here](https://github.com/Sequal32/yourcontrolsinstaller/releases/download/1.2.4/installer.zip)\n\n**Step 3:**\nVerify the paths are correct. **The first path is the install location** (Where you want to launch YourControls from) and the **second path is the location of your MSFS Community folder.**")        
         embed.set_image(url="https://cdn.discordapp.com/attachments/793511228986097727/928425976352894986/YC_Install.png")
-        await ctx.send(embed=embed)
+        await ctx.send(embeds=embed)
 
-    @cog_ext.cog_slash(name="vatsim", description="Shows the instructions on how to use YourControls with Vatsim/IVAO", guild_ids=guild_ids)
+    @interactions.extension_command(name="vatsim", description="Shows the instructions on how to use YourControls with Vatsim/IVAO", scope=guild_ids)
     async def _vatsim(self, ctx):
-        embed=discord.Embed(color=0x00d0ff)
+        embed=interactions.Embed(color=0x00d0ff)
         embed.add_field(name = "__**How To Use YourControls With Vatsim/IVAO**__", value = "If you are looking to use YourControls with either Vatsim or IVAO then check out the instructions [here](https://docs.yourcontrols.one/vatsim-ivao)")
-        await ctx.send(embed=embed)
+        await ctx.send(embeds=embed)
 
-    @cog_ext.cog_slash(name="nomsg", description="Shows possible fixes for the No Message Recived From Server error", guild_ids=guild_ids)
+    @interactions.extension_command(name="nomsg", description="Shows possible fixes for the No Message Recived From Server error", scope=guild_ids)
     async def _nomsg(self, ctx):
-        embed=discord.Embed(color=0x00d0ff)
+        embed=interactions.Embed(color=0x00d0ff)
         embed.add_field(name = "__**No Message Received From Server**__", value = "There are a number of reasons that you may be getting this error. Check both your firewall and router settings and ensure neither of them are blocking YourControls, or that your ISP is blocking the connection. Also, make sure you both have the latest version of YourControls and try with IPv4 P2P and direct.")
-        await ctx.send(embed=embed)
+        await ctx.send(embeds=embed)
 
 def setup(bot):
-    bot.add_cog(help(bot))
+    help(bot)
--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-from email.errors import MessageError
 import os
 from dotenv import load_dotenv
-import discord
 import random
 import asyncio
-from discord import message
-from discord.utils import get
-from discord.ext import commands
-from discord_slash import SlashCommand
-from discord_components import DiscordComponents, ComponentsBot, Button
-from discord_slash.utils.manage_commands import create_option
-from discord_slash.utils.manage_commands import create_permission
-from discord_slash.model import SlashCommandPermissionType
-
+import interactions
+from interactions import Client, Intents
 
 load_dotenv()
-token = os.getenv('TOKEN')
 
-bot = commands.Bot(command_prefix='prefix', intents=discord.Intents.all())
-slash = SlashCommand(bot, sync_commands=True, override_type=True)
+bot = Client(token=os.getenv('TOKEN'), intents=Intents.GUILD_MESSAGE_CONTENT)
 
 guild_ids = [764805300229636107]
 ready = False
@@ -26,17 +15,10 @@ ready = False
 async def changePres():
     await bot.wait_until_ready()
     statuses = ["/help", "Flying With Friends"]
-    await bot.change_presence()
-    while not bot.is_closed():
-        status = random.choice(statuses)
-        await bot.change_presence(
-            status=discord.Status.online,
-            activity=discord.Activity(
-                type=discord.ActivityType.playing,
-                name=f"{status}"))
+    await bot.change_presence(interactions.ClientPresence(activities=[interactions.PresenceActivity(name=random.choice(statuses), type=interactions.PresenceActivityType.GAME)]))
+    await asyncio.sleep(6)
+bot._loop.create_task(changePres())
 
-        await asyncio.sleep(6)
-bot.loop.create_task(changePres())
 
 @bot.event
 async def on_ready():
@@ -47,151 +29,20 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if ("support" in message.content) and ("737" in message.content) and message.author.id != 776052285235003392:
-        await message.reply("Support for the PMDG 737 will come when it comes, if it comes at all. Until then, please be paitient and enjoy of the other aircraft YourControls supports.")
-        
-#Load Cogs
-@bot.command()
-async def load(ctx, extention):
-    bot.load_extension(f'cogs.{extention}')
+        await message.reply("Support for the PMDG 737 will come when it comes, if it comes at all. Until then, please be patient and enjoy of the other aircraft YourControls supports.")        
 
-#Unload Cogs
-@bot.command()
-async def unload(ctx, extention):
-    bot.unload_extension(f'cogs.{extention}')
-
-@slash.slash(name="ping", description="Sends Thingy", guild_ids=guild_ids)
+@bot.event
+async def on_message(message):
+    if ("support" in message.content) and ("737" in message.content) and message.author.id != 776052285235003392:
+        await message.reply("Support for the PMDG 737 will come when it comes, if it comes at all. Until then, please be patient and enjoy of the other aircraft YourControls supports.")        
+    
+@bot.command(name="ping", description="Sends Thingy", scope=guild_ids)
 async def _ping(ctx):
     await ctx.send(f"🌍 Ping is `{round(bot.latency * 1000)}ms`")
 
-@slash.slash(name="kick", description="Remove A Member (They Can Still Rejoin)", guild_ids=guild_ids, options=[
-    create_option(
-        name="member",
-        description="Member to kick",
-        option_type=6,
-        required=True
-    ),
-    create_option(
-        name="reason",
-        description="Reason for kick",
-        option_type=3,
-        required=True
-    ),
-])
-async def _kick(ctx, member: discord.Member, reason: str):
-    embed = discord.Embed(title="Member Kicked", color=0xd92c0d)
-    embed.add_field(
-        name="Member:", value=f"{member.mention}\n**ID**: {member.id}", inline=False)
-    embed.add_field(name="Kicked By:",
-                    value=f"{ctx.author.mention}", inline=False)
-    embed.add_field(name="Reason:", value=f"{reason}", inline=False)
-
-    embed2 = discord.Embed(
-        title=f"You Have Been Kicked from {ctx.guild.name}", color=0xd92c0d)
-    embed2.add_field(name="Reason:", value=f"{reason}", inline=False)
-    embed2.add_field(name="Kicked By:",
-                     value=f"{ctx.author.mention}", inline=False)
-    await member.send(embed=embed2)
-    await member.kick()
-    await ctx.send(embed=embed)
-
-@slash.slash(name="ban", description="Permanently removes a member from the server", guild_ids=guild_ids, options=[
-    create_option(
-        name="member",
-        description="Member to ban",
-        option_type=6,
-        required=True
-    ),
-    create_option(
-        name="reason",
-        description="Reason for ban",
-        option_type=3,
-        required=True
-    ),
-])
-async def _ban(ctx, member: discord.Member, reason: str):
-    embed = discord.Embed(title="Member Banned", color=discord.Color.red())
-    embed.add_field(name="Member:", value=f"{member.mention}\n**ID**: {member.id}", inline=False)
-    embed.add_field(name="Banned By:", value=f"{ctx.author.mention}", inline=False)
-    embed.add_field(name="Reason:", value=f"{reason}", inline=False)
-
-    embed2 = discord.Embed(title=f"You Have Been Banned From {ctx.guild.name}", color=0xd92c0d)
-    embed2.add_field(name="Reason:", value=f"{reason}", inline=False)
-    embed2.add_field(name="Banned By:", value=f"{ctx.author.mention}", inline=False)
-    await member.ban(reason=reason, delete_message_days=1)
-    await ctx.send(embed=embed)
-    await member.send(embed=embed2)
-
-@slash.slash(name="idban", description="Permanently removes a member from the server", guild_ids=guild_ids, options=[
-    create_option(
-        name="id",
-        description="ID of member to ban",
-        option_type=3,
-        required=True
-    ),
-    create_option(
-        name="reason",
-        description="Reason for ban",
-        option_type=3,
-        required=True
-    ),
-])
-async def _idban(ctx, id: str, reason: str):
-    user = await bot.fetch_user(id)
-    embed = discord.Embed(title="Member Banned", color=discord.Color.red())
-    embed.add_field(name="Member:", value=f"{user.mention}\n**ID**: {user.id}", inline=False)
-    embed.add_field(name="Banned By:", value=f"{ctx.author.mention}", inline=False)
-    embed.add_field(name="Reason:", value=f"{reason}", inline=False)
-
-    embed2 = discord.Embed(title=f"You Have Been Banned From {ctx.guild.name}", color=0xd92c0d)
-    embed2.add_field(name="Reason:", value=f"{reason}", inline=False)
-    embed2.add_field(name="Banned By:", value=f"{ctx.author.mention}", inline=False)
-    await ctx.guild.ban(user, reason=reason, delete_message_days=1)
-    await ctx.send(embed=embed)
-    await user.send(embed=embed2)
-
-@slash.slash(name="unban", description="Unbans a member from the server", guild_ids=guild_ids, options=[
-    create_option(
-        name="id",
-        description="ID of member to ban",
-        option_type=3,
-        required=True
-    )
-])
-async def _unban(ctx, id: str):
-    user = await bot.fetch_user(id)
-    embed = discord.Embed(title="Member Unbanned", color=discord.Color.green())
-    embed.add_field(name="Member:", value=f"{user.mention}\n**ID**: {user.id}", inline=False)
-    embed.add_field(name="Unbanned By:", value=f"{ctx.author.mention}", inline=False)
-
-    embed2 = discord.Embed(title=f"You Have Been Unbanned From {ctx.guild.name}", color=0xd92c0d)
-    embed2.add_field(name="Unbanned By:", value=f"{ctx.author.mention}", inline=False)
-    await ctx.guild.unban(user)
-    await ctx.send(embed=embed)
-    await user.send(embed=embed2)
-
-@slash.slash(name="clear", description="Removes a specified number of messages", guild_ids=guild_ids, options=[
-    create_option(
-        name="amount",
-        description="Number of messages to remove",
-        option_type=4,
-        required=True
-    )
-])
-async def _clear(ctx, amount):
-    if amount > 50:
-        embed = discord.Embed(description = f"⚠️ **Please choose a smaller number of messages to remove. (Max 50)**.", color = discord.Color.from_rgb(255, 234, 0))
-        await ctx.send(embed=embed)
-        await asyncio.sleep(6)
-        await ctx.channel.purge(limit=1)
-    else:
-        await ctx.channel.purge(limit=amount)
-        embed = discord.Embed(description = f"♻ **{amount} Messages Removed**.", color = discord.Color.green())
-        await ctx.send(embed=embed)
-        await asyncio.sleep(5)
-        await ctx.channel.purge(limit=1)
-
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+        bot.load(f'cogs.{filename[:-3]}')
+        print(f'Loaded {filename}')
 
-bot.run(token)
+bot.start()
